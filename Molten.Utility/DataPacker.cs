cfa1ff861532db049465b84c9cfc6b7d3677ff48
Mathias Møller Andersen <mathias-m-a@hotmail.com>
2021-11-29 17:02:31 +0100

    Added base for DataPacker

---
+++ Molten.Utility/DataPacker.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Molten
+{
+    internal class DataPacker
+    {
+        List<byte> _bytes;
+        Encoding _encoding;
+
+        public DataPacker()
+        {
+            _bytes = new List<byte>();
+            _encoding = Encoding.UTF8;
+        }
+
+        public DataPacker(Encoding encoding)
+            : this()
+        {
+            _encoding = encoding;
+        }
+
+
+
+        public void Write(string text)
+        {
+            _bytes.AddRange(_encoding.GetBytes(text));
+        }
+
+        public void Write(byte value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(sbyte value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(short value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(ushort value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(int value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(uint value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(long value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(ulong value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(float value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+        public void Write(double value)
+        {
+            _bytes.AddRange(BitConverter.GetBytes(value));
+        }
+
+
+        public byte[] GetData()
+        {
+            return _bytes.ToArray();
+        }
+
+        public void Clear()
+        {
+            _bytes.Clear();
+        }
+    }
+}
